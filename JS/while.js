1f744f9a0ba5fbc707006afc545e67d4089cbb70
kjm541006 <kjm541113@gmail.com>
2022-03-04 18:25:08 +0900

    while

---
+++ JS/while.js
@@ -0,0 +1,15 @@
+// while
+let i = 0;
+while (i < 3) {
+  console.log(i);
+  i++;
+}
+
+console.log("-------------------------------------------");
+
+// do-while
+i = 0;
+do {
+  console.log(i);
+  i++;
+} while (i < 3);
