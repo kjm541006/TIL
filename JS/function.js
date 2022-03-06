45b4c604f9a3d05c128a6cd9bf709a3670eea039
kjm541006 <kjm541113@gmail.com>
2022-03-06 22:28:20 +0900

    function

---
+++ JS/function.js
@@ -0,0 +1,13 @@
+function add(x, y) {
+  return x + y;
+}
+
+const result = add(10, 20);
+console.log(result);
+
+const add1 = (x, y) => {
+  x = 10;
+  y = 20;
+  const result1 = x + y;
+  console.log(result1);
+};
