f35a39baa917816dccbf656c9516e97e4dfc65df
kjm541006 <kjm541113@gmail.com>
2022-03-15 21:37:26 +0900

    Math

--- JS/Math.js
+++ JS/Math.js
@@ -33,9 +33,9 @@
 
   console.log(Math.sqrt(4)); // 2
 
-  console.log(Math.round(3.5)); // 4
-  console.log(Math.ceil(3.5)); // 4
-  console.log(Math.floor(3.5)); // 3
+  console.log(Math.round(3.5)); // 4 (반올림)
+  console.log(Math.ceil(3.5)); // 4 (올림)
+  console.log(Math.floor(3.5)); // 3 (내림)
 
   console.log(Math.round(3.4)); // 3
   console.log(Math.ceil(3.4)); // 4
