0b867972114a29e7fa09341c0ab8af2241b6bc2c
kjm541006 <kjm541113@gmail.com>
2022-03-17 16:14:12 +0900

    배열 뒤집기 loop 1/2로

---
+++ JS_TEST/배열 회전.js
@@ -0,0 +1,36 @@
+/* 8. 배열 회전 */
+
+/* user code */
+function answer(user) {
+  let reverse = [];
+
+  // for (let i = user.length - 1; i >= 0; i--) {
+  //   reverse.push(user[i]);
+  // }
+  let temp;
+  for (let i = 0; i < user.length / 2; i++) {
+    temp = user[i];
+    user[i] = user[user.length - 1 - i];
+    user[user.length - 1 - i] = temp;
+  }
+
+  reverse = user;
+  // reverse = user.reverse();
+
+  return reverse;
+}
+
+/* main code */
+let input = [
+  // TC: 1
+  [1, 2, 3, 4],
+  // TC: 2
+  [-1, 6, "hello", -15],
+  // TC: 3
+  ["apple", "banana", "mango"],
+];
+
+for (let i = 0; i < input.length; i++) {
+  process.stdout.write(`#${i + 1} `);
+  console.log(answer(input[i]));
+}
