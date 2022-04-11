db632605079e2ec8181b0b4524333cfdb88f14f6
kjm541006 <kjm541113@gmail.com>
2022-04-11 22:45:31 +0900

    정해진 수를 배열 원소를 통해 합 구하기

---
+++ JS_TEST/선형 자료구조/TwoSum.js
@@ -0,0 +1,44 @@
+/* two sum */
+
+/* user code */
+function answer(nums, target) {
+  let result = [];
+  // // 2중 for문 (O^2)
+  // for (let i = 0; i < nums.length; i++) {
+  //   for (let j = i + 1; j < nums.length; j++) {
+  //     if (nums[i] + nums[j] == target) {
+  //       result.push(i);
+  //       result.push(j);
+  //       break;
+  //     }
+  //   }
+  // }
+
+  // 단일 for문
+  let map = {};
+  for (let i = 0; i < nums.length; i++) {
+    if (map[target - nums[i]] != undefined) {
+      return [map[target - nums[i]], i];
+    }
+    map[nums[i]] = i;
+  }
+
+  return result;
+}
+
+/* main code */
+let input = [
+  // TC: 1
+  [[2, 7, 11, 15], 9],
+
+  // TC: 2
+  [[3, 2, 4], 6],
+
+  // TC: 3
+  [[3, 3], 6],
+];
+
+for (let i = 0; i < input.length; i++) {
+  process.stdout.write(`#${i + 1} `);
+  console.log(answer(input[i][0], input[i][1]));
+}
