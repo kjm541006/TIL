f3fe9b15f81648455c651a6f781e2bed4f61685d
kjm541006 <kjm541113@gmail.com>
2022-07-26 17:01:20 +0900

    원시타입 state 다루기

--- react/my-app/src/components/Counter.js
+++ react/my-app/src/components/Counter.js
@@ -4,11 +4,34 @@ export default function Counter() {
   // 값, 값을 변경할 떄 쓰는 함수 state -> count, setState -> setCount, 초기값
   // const [set, setState] = useState(초기값);
   const [count, setCount] = useState(0);
+  const [show, setShow] = useState(true);
+
+  const operators = ["+", "-", "*"];
+  const [operator, setOperator] = useState(operators[0]);
   return (
     <div>
-      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button
+        onClick={() => {
+          let result;
+          if (operator === "+") result = count + 1;
+          if (operator === "*") result = count * 1;
+          if (operator === "-") result = count - 1;
+          setCount(result);
+        }}
+      >
+        {operator}1
+      </button>
+      <button onClick={() => setShow(!show)}>Show and Hide</button>
+      <button
+        onClick={() => {
+          const idx = Math.floor(Math.random() * operators.length);
+          setOperator(operators[idx]);
+        }}
+      >
+        Change Operator
+      </button>
       <br />
-      Counter : {count}
+      {show && `Counter : ${count}`}
     </div>
   );
 }
