320d8a9146502baaddf12c7e472ee543bec0a392
kjm541006 <kjm541113@gmail.com>
2022-07-26 17:31:50 +0900

    주석 변경

--- react/my-app/src/components/Counter.js
+++ react/my-app/src/components/Counter.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 export default function Counter() {
-  // 값, 값을 변경할 떄 쓰는 함수 state -> count, setState -> setCount, 초기값
+  // 값 state -> count, 값을 변경할 떄 쓰는 함수 setState -> setCount, 초기값
   // const [set, setState] = useState(초기값);
   const [count, setCount] = useState(0);
   const [show, setShow] = useState(true);
