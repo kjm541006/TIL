44133936912e4594d59e487728ccd58842c37b6c
kjm541006 <kjm541113@gmail.com>
2022-07-29 00:21:27 +0900

    react 중간미션 당첨자 발표

---
+++ react/mission/src/App.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import datas from "./data/cards";
+import BusinessCard from "./components/BusinessCard";
+
+function App() {
+  const [cards, setCards] = useState([]);
+  const [pickedCards, setPickedCards] = useState([]);
+
+  function draw() {
+    if (pickedCards.length > 2) {
+      // alert
+      const names = pickedCards.reduce((acc, cur) => {
+        return (acc = acc.concat(`${cur.name}, `));
+      }, "");
+      return alert(`당첨자는 ${names} 입니다.`);
+    }
+    const randomIdx = Math.floor(Math.random() * cards.length);
+    const randomItem = cards[randomIdx];
+
+    setCards(cards.filter((c) => c.phoneNumber !== randomItem.phoneNumber));
+
+    setPickedCards([...pickedCards, randomItem]);
+  }
+
+  useEffect(() => {
+    setCards(datas);
+  }, []);
+
+  console.log(cards);
+  console.log(pickedCards);
+
+  return (
+    <div>
+      {cards.length > 0 && <button onClick={draw}>추첨하기</button>}
+      {pickedCards.length > 0 && <BusinessCard info={pickedCards[pickedCards.length - 1]} />}
+    </div>
+  );
+}
+
+export default App;
