afd19830f3f19330a55c42f531eb35ff2be8e831
kjm541006 <kjm541113@gmail.com>
2022-07-29 22:23:09 +0900

    중간과제 주석 추가

--- react/mission/src/App.js
+++ react/mission/src/App.js
@@ -7,22 +7,26 @@ function App() {
   const [pickedCards, setPickedCards] = useState([]);
 
   function draw() {
+    // 조건
     if (pickedCards.length > 2) {
-      // alert
       const names = pickedCards.reduce((acc, cur) => {
         return (acc = acc.concat(`${cur.name}, `));
       }, "");
       return alert(`당첨자는 ${names} 입니다.`);
     }
+    // 추첨하기 버튼을 누르면, 랜덤하게 하나의 명함을 고른다.
     const randomIdx = Math.floor(Math.random() * cards.length);
     const randomItem = cards[randomIdx];
 
+    // 중복 제거
     setCards(cards.filter((c) => c.phoneNumber !== randomItem.phoneNumber));
 
+    // 당첨자(arr) 관리
     setPickedCards([...pickedCards, randomItem]);
   }
 
   useEffect(() => {
+    // cards
     setCards(datas);
   }, []);
 
