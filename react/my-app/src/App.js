86e8c6b5c8cdbb93f64e020c82b05a43b9f59035
kjm541006 <kjm541113@gmail.com>
2022-08-02 21:57:30 +0900

    클래스 컴포넌트 이벤트 처리

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -2,12 +2,14 @@
 import FunctionClock from "./components/FunctionClock";
 import UseEffect from "./components/UseEffect";
 import Banner from "./components/Banner";
+import Toggle from "./components/Toggle";
 function App() {
   return (
     <div>
-      <FunctionClock />
-      <UseEffect />
-      <Banner />
+      {/* <FunctionClock /> */}
+      {/* <UseEffect /> */}
+      {/* <Banner /> */}
+      <Toggle />
     </div>
   );
 }
