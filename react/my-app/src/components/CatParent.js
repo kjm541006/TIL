2f3314d914c7b7b9d6ed5dd358502dc16b162dff
kjm541006 <kjm541113@gmail.com>
2022-08-08 17:03:02 +0900

    callback ref

--- react/my-app/src/components/CatParent.js
+++ react/my-app/src/components/CatParent.js
@@ -1,26 +1,49 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import Cat from "./Cat";
 
 export default function CatParent() {
   console.log("부모 컴포넌트 CatParent");
-  const catRef = useRef();
-  console.log(catRef);
+  // const catRef = useRef();
+  // console.log(catRef);
 
-  const ageRef = useRef(1);
-  console.log(ageRef.current);
+  const [height, setHeight] = useState(0);
+  const [age, setAge] = useState(0);
+  const [check, setCheck] = useState(false);
 
-  const [state, setState] = useState(1);
+  const catCallbackRef = (node) => {
+    if (node !== null) {
+      setHeight(node.getBoundingClientRect().height);
+    }
+  };
+
+  useEffect(() => {
+    document.title = `고양이의 나이는 ${age} 입니다.`;
+  }, [age]);
+
+  // const changeAge = () => {
+  //   ageRef.current += 1;
+  //   console.log(`age : ${ageRef.current}`);
+  // };
+
+  const changeAge = () => {
+    setAge((age) => age + 1);
+    console.log(age);
+  };
 
   return (
     <div>
       <h4> 고양이가 세상을 구한다 </h4>
       <div>
-        <Cat ref={catRef} />
-        <h4>나이 : {ageRef.current}</h4>
-        <h4>리렌더링 횟수 : {state}</h4>
-        <button onClick={() => alert(catRef.current.height)}>고양이의 사이즈를 알고싶어</button>
-        <button onClick={() => (ageRef.current += 1)}>나이증가</button>
-        <button onClick={() => setState(state + 1)}>리렌더링</button>
+        <Cat ref={catCallbackRef} />
+        <h4>나이 : {age}</h4>
+        <h4>키 : {check ? height : "비밀"}</h4>
+        {check ? (
+          <button onClick={() => setCheck(false)}>고양이의 키를 숨겨줘</button>
+        ) : (
+          <button onClick={() => setCheck(true)}>고양이의 키를 알고싶어</button>
+        )}
+        <button onClick={changeAge}>나이증가</button>
+        {/* <button onClick={() => setState(state + 1)}>리렌더링</button> */}
       </div>
     </div>
   );
