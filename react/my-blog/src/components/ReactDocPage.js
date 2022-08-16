0db630a52481014eea01b8e69b226b01964e7731
kjm541006 <kjm541113@gmail.com>
2022-08-16 17:08:50 +0900

    json placeholder에서 async await fetch를 사용해 데이터 사용

--- react/my-blog/src/components/ReactDocPage.js
+++ react/my-blog/src/components/ReactDocPage.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function ReactDocPage() {
   const params = useParams();
   console.log(params);
-  return <div>ReactDocPage ##{params.docId}</div>;
+
+  const [paragraph, setParagraph] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.docId}`);
+      console.log(res);
+      const result = await res.json();
+      console.log(result.body);
+      setParagraph(result.body);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>ReactDocPage #{params.docId}</h1>
+      <p>{paragraph}</p>
+    </div>
+  );
 }
--- react/my-blog/src/components/ReactPage.js
+++ react/my-blog/src/components/ReactPage.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, Outlet } from "react-router-dom";
 
 export default function ReactPage() {
-  const docs = [
-    { id: 1, title: "1번입니다.", date: "01/12/2022" },
-    { id: 2, title: "2번입니다.", date: "02/12/2022" },
-    { id: 3, title: "3번입니다.", date: "03/12/2022" },
-    { id: 4, title: "4번입니다.", date: "04/12/2022" },
-    { id: 5, title: "5번입니다.", date: "05/12/2022" },
-  ];
+  // api call
+  // const docs = [
+  //   { id: 1, title: "1번입니다.", date: "01/12/2022" },
+  //   { id: 2, title: "2번입니다.", date: "02/12/2022" },
+  //   { id: 3, title: "3번입니다.", date: "03/12/2022" },
+  //   { id: 4, title: "4번입니다.", date: "04/12/2022" },
+  //   { id: 5, title: "5번입니다.", date: "05/12/2022" },
+  // ];
+  const [docs, setDocs] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      console.log(res);
+      const result = await res.json();
+      console.log(result);
+      setDocs(result);
+    }
+    fetchData();
+  }, []);
 
   return (
     <div>
       {docs.map((doc) => (
-        <Link to={`${doc.id}`} key={doc.id} style={{ display: "block", margin: "1rem 0" }}>
-          {doc.title}
+        <Link to={`${doc.id}`} key={doc.id} style={{ display: "block", margin: "1rem 0", textDecoration: "none" }}>
+          <h2>
+            {doc.id} {doc.title}
+          </h2>
         </Link>
       ))}
       {/* <Outlet /> */}
